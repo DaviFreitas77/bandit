c535f90933ae51c535a359b5ef89a5c57c01d671
Davifreitas77 <davifreitaz999@gmail.com>
2025-06-11 15:30:13 -0300

    add dep

--- src/componentes/banner.tsx
+++ src/componentes/banner.tsx
@@ -1,7 +1,7 @@
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination } from 'swiper/modules';
-import 'swiper/css';
+import 'swiper/swiper-bundle.css';
 import { useEffect } from 'react';
 
 export default function Banner() {
