117e936cf17af684dffc50d16f0fb7b4a0783a3f
Davifreitas77 <davifreitaz999@gmail.com>
2025-06-17 21:15:57 -0300

    add redes no footer

--- src/componentes/footer.tsx
+++ src/componentes/footer.tsx
@@ -1,19 +1,27 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Flex, Text, Link, Icon, Stack } from "@chakra-ui/react";
+import { FaInstagram, FaWhatsapp, FaTiktok } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <Box
-      as="footer"
-      bg="black"
-      color="white"
-      py={4}
-      textAlign="center"
-    
-      bottom="0"
-      width="100%"
-      zIndex="1000"
-    >
-      <Text fontSize="sm">© 2025 YWB SYLISH. Todos os direitos reservados.</Text>
+    <Box as="footer" bg="black" color="white" py={6} px={4} width='100%'>
+      {/* Redes Sociais */}
+      <Flex justify="center" mb={4} gap={6}>
+        <Link href="https://www.instagram.com/y.w.bandit?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" >
+          <Icon as={FaInstagram} boxSize={6} _hover={{ color: "pink.400" }} />
+        </Link>
+        <Link href="https://wa.me/5511954783565" >
+          <Icon as={FaWhatsapp} boxSize={6} _hover={{ color: "green.400" }} />
+        </Link>
+        <Link href="https://www.tiktok.com/@y.w.bstylish?_t=ZM-8xIHn5w4RXk&_r=1" >
+          <Icon as={FaTiktok} boxSize={6} _hover={{ color: "gray.400" }} />
+        </Link>
+      </Flex>
+
+      {/* Texto */}
+      <Stack  textAlign="center">
+        <Text fontSize="sm">© 2025 YWB Stylish. Todos os direitos reservados.</Text>
+        <Text fontSize="xs" color="gray.400">Desenvolvido por sua hypeWeb.</Text>
+      </Stack>
     </Box>
   );
 }
